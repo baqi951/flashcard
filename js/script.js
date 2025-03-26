193efa79d3bb0b22c37df7cfa017769b8b9302c4
baqi adigun <adewunmiadigun08@gmail.com>
2025-03-26 15:02:52 +0100

    add add code to delay scripty

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('.java').click(function () {
+        $('.mean1').slideToggle()
+    })
+    $(".var").click(function () {
+        $(".mean2").slideToggle()
+    })
+    $(".css").click(function () {
+        $(".mean3").slideToggle()
+    })
+    $(".func").click(function () {
+        $(".mean4").slideToggle()
+    })
+    $(".str").click(function () {
+        $(".mean5").slideToggle()
+    })
+    $(".par").click(function () {
+        $(".mean6").slideToggle()
+    })
+    $(".arg").click(function () {
+        $(".mean7").slideToggle()
+    })
+    $(".alt").click(function () {
+        $(".mean8").slideToggle()
+    })
+    $(".ret").click(function () {
+        $(".mean9").slideToggle()
+    })
+   })
